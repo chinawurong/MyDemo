b7481318befa41dbf0a17154cd5df5a0f8fbc9cf
wanglei <mxqlapp@gmail.com>
2014-02-26 15:21:12 +0800

    upadate

--- 自定义Layout/src/com/android/test/diylayout/diyLayout2.java
+++ 自定义Layout/src/com/android/test/diylayout/diyLayout2.java
@@ -3,15 +3,22 @@ package com.android.test.diylayout;
 import java.util.List;
 import java.util.Map;
 
+import net.tsz.afinal.FinalBitmap;
 import net.tsz.afinal.http.AjaxCallBack;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
 import com.clduo.test.rcode.http.HttpBO;
+import com.clduo.test.rcode.utils.DensityUtil;
 import com.clduo.test.rcode.utils.DeviceUtil;
 import com.clduo.test.rcode.utils.JsonUtil;
 
@@ -24,6 +31,7 @@ public class diyLayout2 extends Activity {
 		setContentView(R.layout.activity_layout2);
 		HttpBO.getInitInfo(getScreen(), DeviceUtil.versionName(this), new AjaxCallBack<String>() {
 			public void onSuccess(String result) {
+
 				Map<String, String> baseMap = JsonUtil.getMapStr(result);
 				Map<String, String> resultMap = JsonUtil.getMapStr(baseMap.get("data"));
 				List<Map<String, String>> resultMaps = JsonUtil.getListMapStr(resultMap.get("indexLayout"));
@@ -39,6 +47,13 @@ public class diyLayout2 extends Activity {
 					params.leftMargin = widthP * x;
 					params.topMargin = heightP * y;
 					View itemView = getLayoutInflater().inflate(R.layout.item_layout, null);
+					ImageView imageView = (ImageView) itemView.findViewById(R.id.img);
+					List<String> urls = JsonUtil.getListObj(map.get("picUrl"), String.class);
+
+					FinalBitmap bitmap = FinalBitmap.create(diyLayout2.this);
+					bitmap.display(imageView, urls.get(0));
+					imageView.setOnClickListener(clickListener);
+
 					RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.content);
 					mainLayout.addView(itemView, params);
 				}
@@ -47,12 +62,22 @@ public class diyLayout2 extends Activity {
 
 	}
 
+	private View.OnClickListener clickListener = new OnClickListener() {
+		public void onClick(View v) {
+			ScaleAnimation scaleAnim = new ScaleAnimation(1.0f, 0.95f, 1.0f, 0.95f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+			scaleAnim.setDuration(200);
+			scaleAnim.setRepeatCount(1);
+			scaleAnim.setRepeatMode(Animation.REVERSE);
+			v.startAnimation(scaleAnim);
+		}
+	};
+
 	/** 获取手机分辨率 */
 	private String getScreen() {
 		DisplayMetrics metric = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metric);
 		widthP = metric.widthPixels / 3;
-		heightP = metric.heightPixels / 4;
+		heightP = (metric.heightPixels - DensityUtil.dip2px(this, 150)) / 4;
 		double d = metric.heightPixels / metric.widthPixels;
 		double screen0 = 1.5;
 		double screen1 = 1.67;
