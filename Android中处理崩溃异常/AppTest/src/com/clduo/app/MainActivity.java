9c8996039adf8801cb7e9acda12afabae8ace1a9
wanglei <mxqlapp@gmail.com>
2014-03-14 20:14:21 +0800

    upadte

--- Android中处理崩溃异常/AppTest/src/com/clduo/app/MainActivity.java
+++ Android中处理崩溃异常/AppTest/src/com/clduo/app/MainActivity.java
@@ -2,16 +2,39 @@ package com.clduo.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private String s;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println(s.equals("any string"));
-    }
-    
+	private TextView showCode;
+	private Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		showCode();
+		button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				System.out.println(s.equals("any string"));
+			}
+		});
+	}
+
+	private void showCode() {
+		showCode = (TextView) findViewById(R.id.show_code);
+		StringBuffer sb = new StringBuffer();
+		sb.append("button = (Button) findViewById(R.id.button);").append("\n");
+		sb.append("button.setOnClickListener(new View.OnClickListener() {").append("\n");
+		sb.append("	public void onClick(View v) {").append("\n");
+		sb.append("		System.out.println(s.equals(\"any string\"));").append("\n");
+		sb.append("	}").append("\n");
+		sb.append("});").append("\n");
+		showCode.setText(sb.toString());
+	}
+
 }
--- 异步请求网络/src/com/android/test/asynctask/MainActivity.java
+++ 异步请求网络/src/com/android/test/asynctask/MainActivity.java
@@ -7,7 +7,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
